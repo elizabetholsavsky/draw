855cc24972f51f43725dd2d542136f4771123c30
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-13 18:21:11 -0500

    basic brush drawing on mouse down

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 const canvas = document.querySelector("canvas"),
 ctx = canvas.getContext("2d");
 
-let isDrawing = false;
+let isDrawing = false,
+brushWidth = 5;
 
 window.addEventListener("load", () => {
+    // return viewable w and h off el
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 })
 
 const startDraw = () => {
-    isDrawing = true
-    ctx.beginPath();
+    isDrawing = true;
+    ctx.beginPath(); // begin new path when picking up brush 
+    ctx.lineWidth = brushWidth;
 }
-
 const drawing = (e) => {
     if(!isDrawing) return;
-    ctx.lineTo(e.offsetX, e.offsetY); /* creating line */
-    ctx.stroke(); /* filling line with color */
+    ctx.lineTo(e.offsetX, e.offsetY);  // create line with pointer
+    ctx.stroke();  // fill line with color
 }
 
-canvas.addEventListener("mouseover", startDraw);
-canvas.addEventListener("mouseover", drawing);
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", drawing);
 canvas.addEventListener("mouseup", () => isDrawing = false);
