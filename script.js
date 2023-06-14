131a3b34007ae20553838e15349101f9fa009a2b
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-14 18:44:36 -0500

    add clear btn event listener

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ fillColor = document.querySelector("#fill-color"),
 sizeSlider = document.querySelector("#size-slider"),
 colorBtns = document.querySelectorAll(".colors .option"),
 colorPicker = document.querySelector("#color-picker"),
+clearCanvas = document.querySelector(".clear-canvas"),
 ctx = canvas.getContext("2d");
 
 let prevMouseX, prevMouseY, snapshot,
@@ -103,6 +104,10 @@ colorBtns.forEach(btn => {
 colorPicker.addEventListener("change", () => {
     colorPicker.parentElement.style.background = colorPicker.value;
     colorPicker.parentElement.click();
+});
+
+clearCanvas.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
 canvas.addEventListener("mousedown", startDraw);
